3a77df06dbd202e55d44022e22134eb9ab006d4d
Allan Lopes <allanlps20@gmail.com>
2023-09-12 21:37:13 -0300

    Add files via upload

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,18 +1,15 @@
-let label = document.getElementById("text_itens")
-let input = document.getElementById("item_input")
+// Selecionar o elemento label e input
+var label = document.getElementById("item_label");
+var input = document.getElementById("item_input");
 
-input.addEventListener("focus", setClassInput)
-label.addEventListener("click", setClassLabel)
+// Adicionar ouvinte de eventos para o input ganhar foco
+input.addEventListener("focus", function() {
+    label.classList.add("mudar_cor_label");
+    input.classList.add("mudar_cor_input");
+});
 
-function setClassLabel(){
-    label.classList.toggle("mudar_cor_label")
-   
-}
-
-function setClassInput(){
-        input.classList.toggle("mudar_cor_input")
-}
-
-
-
-
+// Adicionar ouvinte de eventos para o input perder foco
+input.addEventListener("blur", function() {
+    label.classList.remove("mudar_cor_label");
+    input.classList.remove("mudar_cor_input");
+});
