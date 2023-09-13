34b81483ed16536413fc377672b92d7d177a54c9
Allan Lopes <allanlps20@gmail.com>
2023-09-13 10:39:36 -0300

    Update item.js

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,14 +1,14 @@
-// Selecionar o elemento label e input
+// Seleciona o elemento label e input
 var label = document.getElementById("item_label");
 var input = document.getElementById("item_input");
 
-// Adicionar ouvinte de eventos para o input ganhar foco
+// Adiciona ouvinte de eventos para o input ganhar foco
 input.addEventListener("focus", function() {
     label.classList.add("mudar_cor_label");
     input.classList.add("mudar_cor_input");
 });
 
-// Adicionar ouvinte de eventos para o input perder foco
+// Adiciona ouvinte de eventos para o input perder foco ao clicar fora
 input.addEventListener("blur", function() {
     label.classList.remove("mudar_cor_label");
     input.classList.remove("mudar_cor_input");
